dbbed1604fb4d45af736b578f159c93372e37a83
aguedesjr <agsoares@gmail.com>
2016-10-24 17:46:59 -0300

    Ajuste de cores

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
             <div class="col-md-4">
                 <h1>A Empresa</h1>
                 <p>A CST Saraiva  - Consultoria em Segurança do Trabahlo está localizado na cidade de Campos dos Goytacazes/RJ e tem o objetivo de disponibilizar serviços na área de segurança do trabalho e saúde ocupacional.</p>
-                <a class="btn btn-success btn-lg" href="#">Saiba Mais</a>
+                <a href="#" class="btn btn-default btn-lg" style="background-color: #3C763D; color: white;">Saiba Mais</a>
             </div>
             <!-- /.col-md-4 -->
         </div>
@@ -32,36 +32,36 @@
         <div class="row">
         	<div class="col-md-4">
                 <div class="panel panel-success">
-                    <div class="panel-heading">
-                        <h4><i class="fa fa-fw fa-plus-square"></i> Segurança do Trabalho</h4>
+                    <div class="panel-heading" style="background-color: #3C763D;">
+                        <h4 style="color: white;"><i class="fa fa-fw fa-plus-square"></i> Segurança do Trabalho</h4>
                     </div>
                     <div class="panel-body">
                         <p>O servico inicia-se pela elaboracao das Ordens de Servico - OS dando ciencia aos trabalhadores sobre os riscos profissionais nos locais de trabalho, os meios de prevencao e medidas de controle existentes para prevencao.</p>
-                        <a href="#" class="btn btn-success">Saiba Mais</a>
+                        <a href="#" class="btn btn-default" style="background-color: #3C763D; color: white;">Saiba Mais</a>
                     </div>
                 </div>
             </div>
             <!-- /.col-md-4 -->
             <div class="col-md-4">
-                <div class="panel panel-success">
-                    <div class="panel-heading">
-                        <h4><i class="fa fa-fw fa-book"></i> Legislação</h4>
+                <div class="panel panel-default">
+                    <div class="panel-heading" style="background-color: #3C763D;">
+                        <h4 style="color: white;"><i class="fa fa-fw fa-book"></i> Legislação</h4>
                     </div>
                     <div class="panel-body">
                         <p>O ambiente de trabalho possui suas regras próprias que visam garantir a saúde e a segurança das pessoas. No âmbito privado, aplicam-se as Normas Regulamentadoras (NRs) do Ministério do Trabalho e Emprego (MTE).</p>
-                        <a href="legislacao.php" class="btn btn-success">Saiba Mais</a>
+                        <a href="legislacao.php" class="btn btn-default" style="background-color: #3C763D; color: white;">Saiba Mais</a>
                     </div>
                 </div>
             </div>
             <!-- /.col-md-4 -->
             <div class="col-md-4">
                 <div class="panel panel-success">
-                    <div class="panel-heading">
-                        <h4><i class="fa fa-fw fa-line-chart"></i> Treinamentos</h4>
+                    <div class="panel-heading" style="background-color: #3C763D;">
+                        <h4 style="color: white;"><i class="fa fa-fw fa-line-chart"></i> Treinamentos</h4>
                     </div>
                     <div class="panel-body">
                         <p>Uma das principais ferramentas em prevenção de acidentes e doenças relacionadas ao trabalho é a capacitação dos funcionários. A CST Saraiva prepara e ministra treinamentos personalizados, considerando o seu ambiente ocupacional.</p>
-                        <a href="#" class="btn btn-success">Saiba Mais</a>
+                        <a href="#" class="btn btn-default" style="background-color: #3C763D; color: white;">Saiba Mais</a>
                     </div>
                 </div>
             </div>
@@ -72,36 +72,36 @@
         <div class="row">
         	<div class="col-md-4">
                 <div class="panel panel-success">
-                    <div class="panel-heading">
-                        <h4><i class="fa fa-fw fa-stethoscope"></i> Medicina Ocupacional</h4>
+                    <div class="panel-heading" style="background-color: #3C763D;">
+                        <h4 style="color: white;"><i class="fa fa-fw fa-stethoscope"></i> Medicina Ocupacional</h4>
                     </div>
                     <div class="panel-body">
                         <p>O Programa de Controle Médico de Saúde Ocupacional - PCMSO, objetiva o perfeito monitoramento médico da saúde ocupacional dos trabalhadores através dos exames clínicos e complementares necessários.</p>
-                        <a href="#" class="btn btn-success">Saiba Mais</a>
+                        <a href="#" class="btn btn-default" style="background-color: #3C763D; color: white;">Saiba Mais</a>
                     </div>
                 </div>
             </div>
             <!-- /.col-md-4 -->
             <div class="col-md-4">
                 <div class="panel panel-success">
-                    <div class="panel-heading">
-                        <h4><i class="fa fa-fw fa-list"></i> Formulários</h4>
+                    <div class="panel-heading" style="background-color: #3C763D;">
+                        <h4 style="color: white;"><i class="fa fa-fw fa-list"></i> Formulários</h4>
                     </div>
                     <div class="panel-body">
                         <p>Os formulários disponíveis irão ajudar a empresa a se concientizar da importâmcia da segurança do trabalho em seu ambiente, melhorando o desempenho dos empregados.</p><br>
-                        <a href="#" class="btn btn-success">Saiba Mais</a>
+                        <a href="#" class="btn btn-default" style="background-color: #3C763D; color: white;">Saiba Mais</a>
                     </div>
                 </div>
             </div>
             <!-- /.col-md-4 -->
             <div class="col-md-4">
                 <div class="panel panel-success">
-                    <div class="panel-heading">
-                        <h4><i class="fa fa-fw fa-phone"></i> Contato</h4>
+                    <div class="panel-heading" style="background-color: #3C763D;">
+                        <h4 style="color: white;"><i class="fa fa-fw fa-phone"></i> Contato</h4>
                     </div>
                     <div class="panel-body">
                         <p>Entre em contato conosco. Fique por dentro das últimas novidades.</p><br><br><br>
-                        <a href="contact.php" class="btn btn-success">Saiba Mais</a>
+                        <a href="contact.php" class="btn btn-default" style="background-color: #3C763D; color: white;">Saiba Mais</a>
                     </div>
                 </div>
             </div>
