f2a366ba0a45ba026db99f4d7afbcd7b8629d884
aguedesjr <agsoares@gmail.com>
2016-10-25 21:37:20 -0300

    Feito a página falando sobre a empresa.

--- empresa.php
+++ empresa.php
@@ -14,27 +14,46 @@
             </div>
         </div>
         <!-- /.row -->
+        
+        <!-- Image Header -->
+        <div class="row">
+            <div class="col-lg-12">
+                <center><img class="img-responsive" src="img/empresa.png" alt=""></center>
+            </div>
+        </div>
+        <!-- /.row -->
 
         <!-- Blog Post Row -->
-        <div class="row">
+        <!-- <div class="row">
+        	<div class="col-lg-12">
+                <h2 class="page-header"> </h2>
+            </div>
             <div class="col-md-1 text-center">
-                <p><i class="fa fa-camera fa-4x"></i>
-                </p>
-                <p>June 17, 2014</p>
+                <p style="color:#3C763D;">--><!-- <i class="fa fa-building-o fa-4x"></i>  -->
+                <!-- </p>
             </div>
-            <div class="col-md-5">
-                <a href="blog-post.html">
-                    <img class="img-responsive img-hover" src="http://placehold.it/600x300" alt="">
-                </a>
-            </div>
-            <div class="col-md-6">
+            <div class="col-md-8">
                 <h3>
-                    <a href="blog-post.html">Blog Post Title</a>
+                    <p style="color: #3C763D;">Sobre nós</p>
                 </h3>
-                <p>by <a href="#">Start Bootstrap</a>
                 </p>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-                <a class="btn btn-primary" href="blog-post.html">Read More <i class="fa fa-angle-right"></i></a>
+            </div>
+        </div> -->
+        <!-- /.row -->
+
+        <!-- <hr> -->
+
+        <!-- Blog Post Row -->
+        <div class="row">
+            <div class="col-md-1 text-center">
+                <p> <!-- <i class="fa fa-film fa-4x"></i> -->
+                </p>
+            </div>
+            <div class="col-md-8">
+                <h3><p style="color: #3C763D;">Missão</p>
+                </h3>
+                <p>Nossa missão é identificar as necessidades de nossos clientes e prestar serviços de assessoria com qualidade, promovendo a qualidade de vida dos trabalhadores com ações voltadas à segurança, ao meio ambiente e à saúde em sua empresa, de maneira a garantir a satisfação, identificando suas necessidades, propondo soluções adequadas às realidades internas e as legislações específicas, bem como desenvolver treinamentos personalizados, buscando a maximização dos resultados e superar as expectativas.</p>
             </div>
         </div>
         <!-- /.row -->
@@ -44,51 +63,33 @@
         <!-- Blog Post Row -->
         <div class="row">
             <div class="col-md-1 text-center">
-                <p><i class="fa fa-film fa-4x"></i>
+                <p> <!-- <i class="fa fa-film fa-4x"></i> -->
                 </p>
-                <p>June 17, 2014</p>
             </div>
-            <div class="col-md-5">
-                <a href="blog-post.html">
-                    <img class="img-responsive img-hover" src="http://placehold.it/600x300" alt="">
-                </a>
-            </div>
-            <div class="col-md-6">
-                <h3><a href="blog-post.html">Blog Post Title</a>
+            <div class="col-md-8">
+                <h3><p style="color: #3C763D;">Visão</p>
                 </h3>
-                <p>by <a href="#">Start Bootstrap</a>
-                </p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-                <a class="btn btn-primary" href="blog-post.html">Read More <i class="fa fa-angle-right"></i></a>
+                <p>Ser uma empresa especializada e referência em consultoria e treinamentos em segurança do trabalho, saúde ocupacional e meio ambiente. Trabalhar com criatividade e inovação de maneira atender exigências legais e tendências do mercado.</p>
             </div>
         </div>
         <!-- /.row -->
 
         <hr>
-
+        
         <!-- Blog Post Row -->
         <div class="row">
             <div class="col-md-1 text-center">
-                <p><i class="fa fa-file-text fa-4x"></i>
+                <p> <!-- <i class="fa fa-film fa-4x"></i> -->
                 </p>
-                <p>June 17, 2014</p>
             </div>
-            <div class="col-md-5">
-                <a href="blog-post.html">
-                    <img class="img-responsive img-hover" src="http://placehold.it/600x300" alt="">
-                </a>
-            </div>
-            <div class="col-md-6">
-                <h3><a href="blog-post.html">Blog Post Title</a>
+            <div class="col-md-8">
+                <h3><p style="color: #3C763D;">Valores</p>
                 </h3>
-                <p>by <a href="#">Start Bootstrap</a>
-                </p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-                <a class="btn btn-primary" href="blog-post.html">Read More <i class="fa fa-angle-right"></i></a>
+                <p>Ser uma empresa ética, transparente e objetiva, integrada em saúde e segurança do trabalho, prestando serviços de maneira diferenciada, agregando assim, valores para o Clientes.</p>
             </div>
         </div>
         <!-- /.row -->
-
+        
         <hr>
 
         <? include ("footer.php"); ?>
